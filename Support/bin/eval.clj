4dd0b9b3921762f74397d5bc4da51cd0205c1565
David Nolen <david.nolen@nytimes.com>
2010-08-10 22:30:16 -0400

    eval now works as a Clojure shell script. Way more sane.

--- Support/bin/eval.clj
+++ Support/bin/eval.clj
@@ -2,12 +2,14 @@
 (in-ns 'textmate)
 (clojure.core/refer 'clojure.core)
 (require '[clojure.string :as string])
-(load-file (file. (bake/*env* "TM_FILEPATH") "utils.clj"))
-(entire-file-ns)
+(require '[clojure.java.io :as io])
+
+(load-file (str (io/file (bake/*env* "TM_BUNDLE_SUPPORT") "utils.clj")))
+(enter-file-ns)
 
 (clojure.core/require '[clojure.pprint :as pprint])
 (clojure.core/println
   (clojure.core/str 
        "<pre>"
-       ${TM_SELECTED_TEXT:-$TM_CURRENT_LINE}
+       (eval (read-string (bake/*env* "TM_SELECTED_TEXT")))
        "</pre>"))
