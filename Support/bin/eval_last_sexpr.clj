2593e3480f2732c59396a7dae32cdb850aaff83a
David Nolen <david.nolen@gmail.com>
2010-08-13 20:55:19 -0400

    whitespace cleanup. require clojure.java.io as io in eval_last_sexpr.clj

--- Support/bin/eval_last_sexpr.clj
+++ Support/bin/eval_last_sexpr.clj
@@ -1,21 +1,15 @@
 #!/usr/bin/env cake
 (in-ns 'textmate)
 (clojure.core/refer 'clojure.core)
+(require '[clojure.java.io :as io])
 (load-file (str (io/file (bake/*env* "TM_BUNDLE_SUPPORT") "utils.clj")))
-(use 'clojure.java.io)
-;(println "#After: " (text-before-carret) "#")
-;(println "#Form: "  (text-forms (text-after-carret)))
 
 (defn do-stuff []
 	(inc 1)) 
 
 
 (println
- (str 
-   "<pre>"
-   (eval-in-file-ns (clojure.core/eval (get-last-sexpr)))                    
-   "</pre>"))       
-       
-
-
-
+ (str
+    "<pre>"
+    (eval-in-file-ns (clojure.core/eval (get-last-sexpr)))
+    "</pre>"))
--- Support/utils.clj
+++ Support/utils.clj
@@ -5,7 +5,7 @@
 
 (defonce *compiled-files* (atom #{}))
 
-(defn filepath->ns-str 
+(defn filepath->ns-str
   "Convert filepath to ns-str"
   [path]
   (-> path
@@ -13,40 +13,35 @@
       (string/replace "_" "-")
       (string/replace "/" ".")))
 
-
-(defn text-forms 
+(defn text-forms
   "Wrap the forms in text t in a vector. Used
   for all the eval functions"
   [t]
-  (read-string (str "[" t "]")))      
+  (read-string (str "[" t "]")))
 
-
-(defn file-ns 
+(defn file-ns
   "Find the namespace of a file; searches for the first ns  (or in-ns) form
    in the file and returns that symbol. Defaults to 'user if one can't be found"
   []
   (let [forms (-> (bake/*env* "TM_FILEPATH") slurp text-forms)
-        [ns-fn ns] (first (for [f forms :when (and (seq? f) (#{"ns" "in-ns"} (str (first f))))] 
-                    [(first f) (second f)]))]                                                          
-    (if ns  
+        [ns-fn ns] (first (for [f forms :when (and (seq? f) (#{"ns" "in-ns"} (str (first f))))]
+                    [(first f) (second f)]))]
+    (if ns
       (if (= (str ns-fn) "ns") ns (eval ns))
       'user)))
 
-      
-(defn enter-ns 
+(defn enter-ns
   "Enter a ns, wrapped for debugging purposes"
   [ns]
   #_(println (str "Entering " ns))
-  (in-ns ns))              
-
+  (in-ns ns))
 
 (defn enter-file-ns 
   "Enter the ns of the file"
   []
   (let [ns (file-ns)]
-    (enter-ns ns)))     
+    (enter-ns ns)))
 
-    
 (defmacro eval-in-file-ns 
   "For the current file, enter the ns (if any)
   and evaluate the form in that ns, then pop
@@ -56,26 +51,25 @@
     (enter-file-ns)
     (let [r# ~@forms]
       (enter-ns (-> old-ns# str symbol))
-      r#)))            
+      r#)))
 
-      
-(defn project-relative-src-path []  
+(defn project-relative-src-path []
    (let [user-dir (str (bake/*env* "TM_PROJECT_DIRECTORY") "/src/")
         path-to-file (string/replace (bake/*env* "TM_FILEPATH")  user-dir "")]
-  path-to-file))      
-                   
+  path-to-file))
+
 (defn carret-info 
   "returns [path line-index column-index] info
    about current location of cursor"
   []
   [(bake/*env* "TM_FILEPATH")
     (dec (Integer/parseInt (bake/*env* "TM_LINE_NUMBER")))
-    (Integer/parseInt (bake/*env* "TM_LINE_INDEX"))])      
+    (Integer/parseInt (bake/*env* "TM_LINE_INDEX"))])
 
 (defn text-before-carret []
   (let [[path,line-index,column-index] (carret-info)
         lines (-> path io/reader line-seq)]
-     (apply str 
+     (apply str
        (apply str (for [l (take line-index lines)] (str l "\n")))
        (.substring #^String (nth lines line-index) 0 column-index))))    
 
@@ -102,14 +96,10 @@
    #_(println "Last SEXPR" last)
    last))
 
-  
-(defn get-selected-sexpr 
+(defn get-selected-sexpr
   "Get highlighted sexpr"
   []
   (-> "TM_SELECTED_TEXT" bake/*env* clojure.core/read-string))
-  
-
 
 (defn get-enclosing-sexpr [])
-
 (defn get-current-symbol [])
